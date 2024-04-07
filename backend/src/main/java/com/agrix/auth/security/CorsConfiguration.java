8181e1e7cc79943c7dfad0f5b9818edb5e695cc5
joseg-alvesg <cinhoalves85@gmail.com>
2024-04-07 18:56:32 -0300

    update cors

--- backend/src/main/java/com/agrix/auth/security/CorsConfiguration.java
+++ backend/src/main/java/com/agrix/auth/security/CorsConfiguration.java
@@ -1,17 +1,19 @@
-package com.agrix.security;
+package com.agrix.auth.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class to enable CORS for the application.
+ */
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-  }
 
+  }
 }
