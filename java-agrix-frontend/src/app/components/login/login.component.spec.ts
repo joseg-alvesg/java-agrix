dbf8ae56b8e84f1cabe255a3cfa98ce66f66dd3d
joseg-alvesg <cinhoalves85@gmail.com>
2024-04-09 13:34:48 -0300

    update tree fix

--- java-agrix-frontend/src/app/components/login/login.component.spec.ts
+++ java-agrix-frontend/src/app/components/login/login.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LoginComponent } from './login.component';
+import { LoginComponent } from '../../modules/login/components/login/login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,10 +7,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    })
-    .compileComponents();
-    
+      declarations: [LoginComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
